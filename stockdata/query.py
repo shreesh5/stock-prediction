d5dc7d7cd348d92d4edddb7bd3e3e8dc923e7aaf
rohitkpa <rohitkpa@gmail.com>
2020-02-12 18:22:40 -0700

    switched to daily

--- stockdata/query.py
+++ stockdata/query.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 
 def query():
     ts = TimeSeries(key='G89M0HECMYPKA46T', output_format='pandas')
-    data, meta_data = ts.get_intraday('AAPL', interval='60min', outputsize='full')
-    pprint(data.head(100))  # prints the most recent 100 points
+    data, meta_data = ts.get_daily('TSLA', outputsize='full')
+    pprint(data)  # prints the most recent 100 points
     data['4. close'].plot()
-    plt.title('Intraday (60 min) AAPL')
+    plt.title('Daily TSLA')
     plt.show()
 
 
